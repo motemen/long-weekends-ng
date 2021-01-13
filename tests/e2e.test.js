f32a21cdf39a5f289e9eaf64b81ce10f0f60fc2f
motemen <motemen@gmail.com>
2021-01-13 22:40:54 +0900

    test

--- tests/e2e.test.js
+++ tests/e2e.test.js
@@ -3,23 +3,30 @@ const PORT = process.env['PORT'] || 8383;
 describe('index', () => {
   const url = `http://localhost:${PORT}/holidays.html`;
 
-  it('普通の日 2018-04-01', async () => {
+  it('普通の日', async () => {
     const page = await browser.newPage();
-    await page.goto(url + '#2018-04-01');
+    await page.goto(url);
     await page.waitForSelector('#lead-text');
-    await expect(page).toMatch('次の連休は 27 日後です');
+    await expect(page).toMatch(/次の連休は \d+ 日後です/);
   });
 
-  it('連休発生中 2018-01-01', async () => {
+  it('普通の日 2021-12-01', async () => {
     const page = await browser.newPage();
-    await page.goto(url + '#2018-01-01');
+    await page.goto(url + '#2021-12-01');
+    await page.waitForSelector('#lead-text');
+    await expect(page).toMatch('次の連休は 38 日後です');
+  });
+
+  it('連休発生中 2021-01-01', async () => {
+    const page = await browser.newPage();
+    await page.goto(url + '#2021-01-01');
     await page.waitForSelector('marquee');
     await expect(page).toMatch('連休発生中です！！！');
   });
 
-  it('有給チャンスを教えてくれる 2017-11-06', async () => {
+  it('有給チャンスを教えてくれる 2021-01-13', async () => {
     const page = await browser.newPage();
-    await page.goto(url + '#2017-11-06');
-    await expect(page).toMatch('次の連休は 54 日後ですが、2017-11-24 に有給を取れば 17 日後に連休チャンス！');
+    await page.goto(url + '#2021-01-13');
+    await expect(page).toMatch('次の連休は 108 日後ですが、2021-02-12 に有給を取れば 29 日後に連休チャンス！');
   });
 });
